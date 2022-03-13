88a45e0db6aa41e5826342ffb4f8a2e813612311
AuroraPortesine <100139517+AuroraPortesine@users.noreply.github.com>
2022-03-13 18:05:58 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 
 int main(){
-//Qui vanno definite le variabili
-
+float a=0; float b=0; float c=0;
+float Areatriangolo=((a*b)/2); float Areaquadrato=(a*a); float Arearettangolo=(a*b); float Areatrapezio=(((a+b)*c)/2);
 cin >> a >> b >> c;
 
-//Qui va scritto il codice per eseguire i calcoli richiesti
-
-
+std::cout << ((a*b)/2) <<std::endl;
+std::cout << (a*a) <<std::endl;
+std::cout << (a*b) <<std::endl;
+std::cout << (((a+b)*c)/2) <<std::endl;
+return 0;
 }
